212c66459d8a6114a3090bd05263c346155830c1
janidtk <134063478+janidtk@users.noreply.github.com>
2023-05-24 09:33:14 +0200

    main.blocks, main.ts frissítése

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     led.plot(0, 0)
 })
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(0)
+})
 basic.showLeds(`
     # . . . #
     # # . # #
